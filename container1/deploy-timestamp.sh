cd89b0116b02d48aa240e50d5021e6f906d2a0e6
nlejeune <nlejeune@flashreport.io>
2019-08-14 11:22:03 +0200

    Scripts to deploy based on a timestamp tag

---
+++ container1/deploy-timestamp.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+NAME=lejeunen/container1
+TAG='t'$(date +%Y%m%d%H%M%S)
+IMAGE=${NAME}:${TAG}
+LATEST=${NAME}:latest
+
+echo ${IMAGE}
+echo ${LATEST}
+
+docker build -t ${IMAGE} .
+docker tag ${IMAGE} ${LATEST}
+docker push ${IMAGE}
+docker push ${LATEST}
+
+echo
+helm upgrade container1 ../system/container1/ --set image.tag=${TAG},image.repository=${NAME}
+echo
---
+++ container3/deploy-timestamp.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+NAME=lejeunen/container3
+TAG='t'$(date +%Y%m%d%H%M%S)
+IMAGE=${NAME}:${TAG}
+LATEST=${NAME}:latest
+
+echo ${IMAGE}
+echo ${LATEST}
+
+docker build -t ${IMAGE} .
+docker tag ${IMAGE} ${LATEST}
+docker push ${IMAGE}
+docker push ${LATEST}
+
+echo
+helm upgrade container3 ../system/container3/ --set image.tag=${TAG},image.repository=${NAME}
+echo
