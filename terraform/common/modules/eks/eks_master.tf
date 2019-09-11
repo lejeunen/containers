7520e4a1181a183b250eba5345b276315324e32b
nlejeune <nlejeune@flashreport.io>
2019-09-11 15:42:17 +0200

    Target kubernetes 1.14

--- terraform/common/modules/eks/eks_master.tf
+++ terraform/common/modules/eks/eks_master.tf
@@ -1,10 +1,12 @@
 resource "aws_eks_cluster" "this" {
-  name            = var.cluster_name
-  role_arn        = aws_iam_role.eks_master.arn
+  name = var.cluster_name
+  role_arn = aws_iam_role.eks_master.arn
+  version = 1.14
 
   vpc_config {
-    security_group_ids = [aws_security_group.eks_master.id]
-    subnet_ids         = var.app_subnet_ids
+    security_group_ids = [
+      aws_security_group.eks_master.id]
+    subnet_ids = var.app_subnet_ids
   }
 
   depends_on = [
