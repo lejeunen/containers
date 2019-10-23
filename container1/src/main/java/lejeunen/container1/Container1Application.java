c9de3a5dc4de6babd5316b6cbbf168f56b733e13
nlejeune <lejeune.n@gmail.com>
2019-10-23 17:02:55 +0200

    Dummy endpoints to test inter service communications

--- container1/src/main/java/lejeunen/container1/Container1Application.java
+++ container1/src/main/java/lejeunen/container1/Container1Application.java
@@ -2,12 +2,33 @@ package lejeunen.container1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@RestController
 public class Container1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Container1Application.class, args);
 	}
 
+	@GetMapping("hello")
+	public String hello(){
+		return "Hello";
+	}
+	@GetMapping("hello/world")
+	public String helloWorld(){
+		return "Hello World";
+	}
+	@GetMapping("call/module2")
+	public String callModule2(){
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response
+				= restTemplate.getForEntity("http://module2-container2.emasphere.svc.cluster.local:9000/hello" , String.class);
+
+		return response.toString();
+	}
 }
